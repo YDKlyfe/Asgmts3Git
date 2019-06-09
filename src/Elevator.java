81289bb05eed3edbfb8c388724440406277168f2
Yaa Karikari <50807568+YDKlyfe@users.noreply.github.com>
2019-06-08 23:41:44 -0500

    Update Elevator.java

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,4 +1,3 @@
-//import java.io.InputStream;
 import java.util.*;
 
 public class Elevator {
@@ -6,15 +5,11 @@ public class Elevator {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		//Initial conditions
-		char state = 'C';
-		char floor = 'B';
-		
-// First, we have only one elevator in a three-story building (three floors): the Basement (B), the first floor (1) and the second floor (2)
-			
+        // First, we have only one elevator in a three-story building (three floors): 
+        // the Basement (B), the first floor (1) and the second floor (2)
 	    
 	    //Print of initial state
-	    System.out.println("The elevator (E) is in 'B'");
+	    System.out.println("The elevator (E) is in 'Basement'");
 	    
 	    //FIRST: Call the elevator from a floor
 		System.out.println("Call the elevator");
@@ -32,58 +27,78 @@ public class Elevator {
 		    System.out.println("The Elevator (E) is now at '1'");
 		    System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
 		}
-        else if (call == '2')
+        else
 	    {
 	        System.out.println("The Elevator (E) is going up '2U'");
 	        System.out.println("The Elevator (E) is now at '2'");
 	        System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
 	    }
-
-        //If elevator is at 1 floor 
-		System.out.println("Call the elevator");
-		System.out.println("Enter the floor you call it from (B,1,2):"); 
-		char call1 = sc.next().charAt(0);
+	    
+	    //Now, we press the button of the floor we're going to
+	    System.out.println("Press the destination floor");
+		System.out.println("Enter the floor you want to go to (B,1,2):"); 
+		char dest = sc.next().charAt(0);
 		
-		if(call1 == '1')
-		{	
-			System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
-			System.out.println("Elevator is at the First floor");
-		}
-		else if(call1 == '2')
+		if(dest == 'B')
 		{
-			System.out.println("The Elevator (E) is going up '2U'");
-	        System.out.println("The Elevator (E) is now at '2'");
-	        System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
+		    if(call == 'B')
+		    {
+		        System.out.println("The Elevator (E) is already at 'B'");
+		    }
+		    else if (call == '1')
+		    {
+		        System.out.println("The Elevator (E) is going down 'D'");
+		        System.out.println("The Elevator (E) is now at 'B'");
+		    }
+		    else
+		    {
+		        System.out.println("The Elevator (E) is going down '2D'");
+	            System.out.println("The Elevator (E) is now at 'B'");
+		    }
+		    System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
 		}
-		else if(call1 == 'B')
+		else if (dest == '1')
 		{
-			System.out.println("The Elevator (E) is going down 'D'");
-	        System.out.println("The Elevator (E) is now at 'B'");
-	        System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
+		    if(call == '1')
+		    {
+		        System.out.println("The Elevator (E) is already at '1'");
+		    }
+		   
+    		else if (call == 'B')   
+    		{
+    		    System.out.println("The Elevator (E) is going up 'U'");
+		        System.out.println("The Elevator (E) is now at '1'");
+    		}
+    		else
+    		{
+    		    System.out.println("The Elevator (E) is going down 'D'");
+	            System.out.println("The Elevator (E) is now at '1'");
+    		}
+    		System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
 		}
-        	 
-		// If elevator is at 2 floor 
-		System.out.println("Call the elevator");
-		System.out.println("Enter the floor you call it from (B,1,2):"); 
-		char call2 = sc.next().charAt(0);
-		
-		if(call2 == '2')
+		else if (dest == '2')
 		{
-			System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
-			System.out.println("Elevator is at the Second floor");
-		}
-		else if(call2 == '1')
-		{
-			System.out.println("The Elevator (E) is going down 'D'");
-	        System.out.println("The Elevator (E) is now at '1'");
-	        System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
-		}
-		else if(call2 == 'B')
-		{
-			System.out.println("The Elevator (E) is going down '2D'");
-	        System.out.println("The Elevator (E) is now at 'B'");
-	        System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
+		    if (call == '2')
+		    {
+		        System.out.println("The Elevator (E) is already at '2'");
+		    }
+		    else if (call == '1')
+		    {
+		        System.out.println("The Elevator (E) is going down 'D'");
+	            System.out.println("The Elevator (E) is now at '1'");
+		    }
+		    else 
+		    {
+		        System.out.println("The Elevator (E) is going down '2D'");
+	            System.out.println("The Elevator (E) is now at 'B'");
+		    }
+		    System.out.println("The Elevator (E) is now ‘open ‘O’ and ‘C'");
 		}
 	}
-
 }
+	
+
+	
+	
+
+
